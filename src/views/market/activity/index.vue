3c88e84c8cad905976ea2dae5ff184ac108af2e7
dd <1751919770@qq.com>
2021-01-14 11:37:48 +0800

    修改营销管理时间bug

--- src/views/market/activity/index.vue
+++ src/views/market/activity/index.vue
@@ -48,8 +48,8 @@
             <p>
               {{
                 isJudgeTime(
-                  scope.row.promotionEndTime,
-                  scope.row.promotionStartTime
+                  scope.row.promotionStartTime,
+                  scope.row.promotionEndTime
                 )
               }}
             </p>
@@ -100,7 +100,12 @@
       </el-table>
     </el-card>
     <!-- dialog对话框 -->
-    <el-dialog title="热销详情" :visible.sync="dialogVisible" width="80%"   style="margin-top: -100px">
+    <el-dialog
+      title="热销详情"
+      :visible.sync="dialogVisible"
+      width="80%"
+      style="margin-top: -100px"
+    >
       <div class="dialog-main">
         <el-card class="main" shadow="never">
           <div slot="header">
@@ -171,7 +176,7 @@
           </el-row>
         </el-card>
         <!-- 商品列表 -->
-        <el-card class=" table_main" shadow="never">
+        <el-card class="table_main" shadow="never">
           <div slot="header">
             <span>商品列表</span>
           </div>
@@ -221,8 +226,8 @@
                 align="center"
               >
               </el-table-column>
-                <el-table-column prop="weight" label="重量" align="center" >
-        </el-table-column>
+              <el-table-column prop="weight" label="重量" align="center">
+              </el-table-column>
             </el-table>
           </el-radio-group>
           <el-pagination
@@ -253,7 +258,6 @@
         >
           <el-col :span="12" :offset="0">
             <el-form-item label="商品原价">
-            
               <span>{{ productDetail.price }}</span>
             </el-form-item>
           </el-col>
@@ -307,18 +311,16 @@
                 style="width: 150px; height: 150px"
                 :src="productDetail.pic"
                 alt=""
-              >
+              />
             </el-form-item>
           </el-col>
         </el-form>
       </el-row>
       <span slot="footer">
         <el-button @click="isEditOpen = false">取消</el-button>
-        <el-button
-          type="primary"
-          :loading="couldNext"
-          @click="onSubmit"
-        >确定</el-button>
+        <el-button type="primary" :loading="couldNext" @click="onSubmit"
+          >确定</el-button
+        >
       </span>
     </el-dialog>
   </div>
@@ -328,191 +330,188 @@ import {
   addRecommend,
   delRecommend,
   findAllRecommends,
-  updateRecommend,
-} from "@/api/market/index";
-import { brandList } from "@/api/product/brand/index";
-import { productList } from "@/api/product/index";
-import mixin from "@/mixins/index";
+  updateRecommend
+} from '@/api/market/index'
+import { brandList } from '@/api/product/brand/index'
+import { productList } from '@/api/product/index'
+import mixin from '@/mixins/index'
 export default {
   mixins: [mixin],
   data() {
     return {
-       // 是否显示
-            isEditOpen: false,
+      // 是否显示
+      isEditOpen: false,
       loading: false,
       list: [],
       dialogVisible: false, //对话框
       search: {
         //条件查询
-        name: "",
-        productSn: "",
-        brandId:'',
-        publishStatus: "",
-        verifyStatus: "",
+        name: '',
+        productSn: '',
+        brandId: '',
+        publishStatus: '',
+        verifyStatus: ''
       },
       // 品牌列表
       brandList: [],
       // 商品列表
       productList: [],
-       couldNext: false, // 是否可以下一步
-      productId: "", // radio选中 商品id
-       form: {
-                productId: '', // 商品ID
-                promotionPrice: '',
-                promotionPerLimit: '',
-                sort: '',
-                promotionStartTime: '',
-                promotionEndTime: ''
-            },
-        productDetail: {} // 编辑商品的详情
-
-    };
+      couldNext: false, // 是否可以下一步
+      productId: '', // radio选中 商品id
+      form: {
+        productId: '', // 商品ID
+        promotionPrice: '',
+        promotionPerLimit: '',
+        sort: '',
+        promotionStartTime: '',
+        promotionEndTime: ''
+      },
+      productDetail: {} // 编辑商品的详情
+    }
   },
   created() {
-    this.init();
-    this.getBrandList();
-    this.getProductList();
+    this.init()
+    this.getBrandList()
+    this.getProductList()
   },
   methods: {
     init() {
-      this.loading = true;
+      this.loading = true
       findAllRecommends().then((res) => {
-        console.log(res);
+        console.log(res)
         if (res.success) {
-          this.list = res.data.items;
+          this.list = res.data.items
         }
-        this.loading = false;
-      });
+        this.loading = false
+      })
     },
     //   获取商品列表
     getProductList() {
-      this.loading = true;
+      this.loading = true
       productList(this.page.currentPage, this.page.size, this.search).then(
         (res) => {
-          console.log('商品列表',res);
-          this.productList = res.data.rows;
-          this.page.total = res.data.total;
-          this.loading = false;
+          console.log('商品列表', res)
+          this.productList = res.data.rows
+          this.page.total = res.data.total
+          this.loading = false
         }
-      );
+      )
     },
     // 获取品牌列表
     getBrandList() {
       brandList().then((res) => {
-        console.log('品牌列表',res);
-        this.brandList = res.data.items;
-      });
+        console.log('品牌列表', res)
+        this.brandList = res.data.items
+      })
     },
     // 重置
     doReset() {
-      this.search = this.$options.data().search;
+      this.search = this.$options.data().search
     },
     // 搜索
     doSearch() {
-       this.page.currentPage = 1
-        this.getProductList()
+      this.page.currentPage = 1
+      this.getProductList()
     },
     del(row) {
-      console.log(row);
-      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
+      console.log(row)
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
       })
         .then(() => {
           delRecommend(row.recommendId).then((res) => {
             if (res.success) {
-              this.init();
+              this.init()
               this.$message({
-                type: "success",
-                message: "删除成功!",
-              });
+                type: 'success',
+                message: '删除成功!'
+              })
             } else {
-              this.$message.error(res.message);
+              this.$message.error(res.message)
             }
-          });
+          })
         })
         .catch((rej) => {
-          console.log(rej);
-        });
+          console.log(rej)
+        })
     },
     radioChange(val) {
       // console.log(val); //当前商品的id
-       this.isEditOpen = true
-            this.productList.forEach((item) => {
-                if (item.id === val) {
-                    this.productDetail = Object.assign({}, item)
-                }
-            })
-            this.form = {}
+      this.isEditOpen = true
+      this.productList.forEach((item) => {
+        if (item.id === val) {
+          this.productDetail = Object.assign({}, item)
+        }
+      })
+      this.form = {}
 
-            this.productId = val
+      this.productId = val
     },
     // 是否过期
     isJudgeTime(startTime, endTime) {
-      var num = new Date().getTime() - new Date(endTime).getTime();
-      return num > 0 ? "过期" : "否";
+      var num = new Date().getTime() - new Date(endTime).getTime()
+      return num > 0 ? '过期' : '否'
     },
     // 改变每页显示数量
     handleSizeChange(val) {
-      this.page.size = val;
-      this.page.currentPage = 1;
-      this.getProductList();
+      this.page.size = val
+      this.page.currentPage = 1
+      this.getProductList()
     },
     // 切换当前页码
     handleCurrentChange(val) {
-      this.page.currentPage = val;
-      this.getProductList();
+      this.page.currentPage = val
+      this.getProductList()
     },
-  // 提交
-        onSubmit() {
-            this.$confirm(`是否确认新增最热推荐商品?`, '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning'
-            })
-                .then(() => {
-                    console.log('ss')
-                    this.couldNext = true // 打开btn  loding
-                   addRecommend({
-                            ...this.form,
-                            productId: this.productId
-
-                        })
-                        .then((res) => {
-                            console.log('res', res)
-                            if (res.success) {
-                                this.isEditOpen = false // 关闭编辑弹窗
-                                this.dialogVisible = false // 关闭新增弹窗
-                                this.productId = ''
-                                this.$emit('success')
-                                this.$message.success('添加成功')
-                            } else {
-                                this.$message.error('添加失败')
-                            }
-                            this.couldNext = false
-                        })
-           
-                })
-                .catch((rej) => {
-                    console.log('quxiao')
-                })
-        }
-  },
-};
+    // 提交
+    onSubmit() {
+      this.$confirm(`是否确认新增最热推荐商品?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          console.log('ss')
+          this.couldNext = true // 打开btn  loding
+          addRecommend({
+            ...this.form,
+            productId: this.productId
+          }).then((res) => {
+            console.log('res', res)
+            if (res.success) {
+              this.init()
+              this.isEditOpen = false // 关闭编辑弹窗
+              this.dialogVisible = false // 关闭新增弹窗
+              this.productId = ''
+              this.$emit('success')
+              this.$message.success('添加成功')
+            } else {
+              this.$message.error('添加失败')
+            }
+            this.couldNext = false
+          })
+        })
+        .catch((rej) => {
+          console.log('quxiao')
+        })
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
-@import "@/styles/myScss.scss";
-.dialog-main{
+@import '@/styles/myScss.scss';
+.dialog-main {
   position: relative;
   height: 700px;
   overflow: auto;
 }
- .table_main {
-   margin-top: 20px;
-    height: 500px;
-    overflow: auto;
-  }
+.table_main {
+  margin-top: 20px;
+  height: 500px;
+  overflow: auto;
+}
 .table_img {
   width: 100px;
   height: 100px;
