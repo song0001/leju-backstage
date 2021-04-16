37e0a0595cc2c3dd7d48c746c9e8d10012751689
Spark <1751919770@qq.com>
2021-04-16 14:01:20 +0800

    打包

--- src/settings.js
+++ src/settings.js
@@ -1,20 +1,20 @@
 module.exports = {
 
-    title: '乐居商城后台管理系统',
+  title: '乐居商城后台管理系统',
 
-    /**
+  /**
      * @type {boolean} true | false
      * @description Whether fix the header
      */
-    fixedHeader: false,
+  fixedHeader: false,
 
-    /**
+  /**
      * @type {boolean} true | false
      * @description Whether show the logo in sidebar
      */
-    sidebarLogo: false,
-    // 定义一个host
-    // host: 'http://localhost'
-        // host: 'http://192.160.0.100'
-        host: 'http://leju.bufan.cloud'
+  sidebarLogo: false,
+  // 定义一个host
+  // host: 'http://localhost'
+  // host: 'http://192.160.0.100'
+  host: 'http://leju.bufan.cloud'
 }
